c757b3664d215117d92f97fcf8eb6dd366bd8aaf
yashexperrt17 <vu4f2122004@pvppcoe.ac.in>
2024-10-03 20:16:48 +0530

    network issues solved

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,12 +1,15 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = (onPerformanceEntry) => {
+  if (typeof onPerformanceEntry === 'function') {
+    import('web-vitals')
+      .then((webVitals) => {
+        const { getCLS, getFID, getFCP, getLCP, getTTFB } = webVitals;
+        const vitalsMetrics = [getCLS, getFID, getFCP, getLCP, getTTFB];
+
+        vitalsMetrics.forEach((metric) => metric(onPerformanceEntry));
+      })
+      .catch((error) => {
+        console.error('Error importing web-vitals:', error);
+      });
   }
 };
 
